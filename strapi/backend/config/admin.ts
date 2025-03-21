a1b80543e6d8a16bbd8d38bfd7b5f4dc532ae32c
dimitrijes5 <dimitrije@mxd.digital>
2025-03-21 13:38:57 +0100

    Inserting dummy contact data

--- strapi/backend/config/admin.ts
+++ strapi/backend/config/admin.ts
@@ -1,13 +1,13 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'f7e8d6c5b4a3921087654321dcba9876'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'a8346c9d5f2e7b1908d63f4a52c71b9e'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', '1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p'),
     },
   },
   flags: {
--- strapi/backend/config/server.ts
+++ strapi/backend/config/server.ts
@@ -2,6 +2,6 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS') || ['randomKey14dca8972fb', 'randomKey2bf6534e89c', 'randomKey37a9d21e56f', 'randomKey48c3a591e7d'],
   },
 });
